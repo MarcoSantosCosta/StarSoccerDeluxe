c2f5a51f7bc324dae87e7c8b055524f73e3da82b
SantosWell <sr-wellington@hotmail.com>
2018-02-04 22:28:59 -0200

    Atualizações mobile, e pages

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,34 @@
 $(document).ready(function(){
-    $('ul.tabs').tabs({
+  $('ul.tabs').tabs({
     swipeable : true   
-    });
+  });
 });
 
 
 function initMap() {
-    var comp = {lat: -21.227783, lng: -44.978517};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 18,
-        center: comp
-    });
-    var marker = new google.maps.Marker({
+  var comp = {lat: -21.227783, lng: -44.978517};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: comp
+  });
+  var marker = new google.maps.Marker({
     position: comp,
     map: map
-    });
+  });
 }
 
 
 $(document).ready(function(){
-    $('.carousel').carousel();
+  $('.carousel').carousel();
 });
-        
 
-  $(document).ready(function() {
-    $('select').material_select();
-  });
-           
- $('select').material_select('destroy');
-            
+
+$(document).ready(function() {
+  $('select').material_select();
+});
+
+$('select').material_select('destroy');
+
 
 
 function scrollto (target) {
@@ -36,8 +36,27 @@ function scrollto (target) {
 }
 
 $(document).ready(function(){
-     $('.slider').slider();
-     $('.slider').height ='900px';
+ $('.slider').slider();
+ $('.slider').height ='900px';
 });
 
-        
+
+var options = [
+{selector: '.botoes-icon', offset: 200, callback: customCallbackFunc },
+{selector: '.botoes-icon', offset: 200, callback: function() {
+  customCallbackFunc();
+} },
+];
+Materialize.scrollFire(options);
+
+$('.dropdown-button').dropdown({
+  inDuration: 300,
+  outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+    );
